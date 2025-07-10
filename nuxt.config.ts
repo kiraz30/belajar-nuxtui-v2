e9b5d7aab11435e92ad73cf35cf1cffd4196bb81
Bima Setya Nugraha <setyabima016@gmail.com>
2025-07-10 11:17:53 +0700

    init tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  css: ['@/assets/css/main.css'],
   modules: [
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss' // harus ada ini
   ],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
+  css: ['@/assets/css/main.css'],
+  tailwindcss: {
+    viewer: true // pastikan ini true
   }
 })
