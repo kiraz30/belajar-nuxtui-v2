70a08a1bc79420f8052f650ac0cb238fd47bd3d0
Bima Setya Nugraha <setyabima016@gmail.com>
2025-07-10 11:11:49 +0700

    init project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+     <UButton :disabled="loading" @click="handleSubmit">
+        Submit Data
+    </UButton>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
